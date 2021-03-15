dbde9b86f7360df11a2081579673fa6e31dcd718
Mason Fox <masonfox22@gmail.com>
2021-03-15 08:47:06 -0400

    fix header spacing issue

--- App/components/Budget.vue
+++ App/components/Budget.vue
@@ -6,7 +6,7 @@
         <div class="flex items-center border-b border-gray-200 border-t px-4 py-2">
             <!-- Budget Header -->
             <input name="selectAllCategories" type="checkbox" class="h-4 mr-5">
-            <span class="budget-header w-1/3">Category</span>
+            <span class="budget-header w-1/2">Category</span>
             <span class="budget-header text-right w-1/6">Budgeted</span>
             <span class="budget-header text-right w-1/6">Activity</span>
             <span class="budget-header text-right w-1/6">Available</span>
