3d7dd49ea348a953b70e1dfe797344b1a4d8b764
Mason Fox <masonfox22@gmail.com>
2021-03-15 04:06:18 -0400

    added fontawesome

--- App/nuxt.config.js
+++ App/nuxt.config.js
@@ -35,6 +35,16 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    ['nuxt-fontawesome', {
+      component: 'fa',
+      imports: [
+        //import whole set
+        {
+          set: '@fortawesome/free-solid-svg-icons',
+          icons: ['fas']
+        },
+      ]
+    }],
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
